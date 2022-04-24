108f682fe31d1c6eceba7bdc9532e8292a4bfdf1
Yevhenii Maksimishyn <evgenijmaksimishin@gmai.com>
2022-04-24 23:26:13 +0200

    Create data variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DropDowmMenuComponent } from './components/drop-dowm-menu/drop-dowm-menu.component';
+import { DropDownMenuData } from './interfaces/drop-down-menu';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'drop-down-menu';
+export class AppComponent implements OnInit {
+  public selectData_1: DropDownMenuData[] = [
+    { name: 'selectData_1', id: 1 },
+    { name: 'selectData_2', id: 2 },
+    { name: 'selectData_3', id: 3 },
+    { name: 'selectData_4', id: 4 }
+  ];
+  public selectData_2: DropDownMenuData[] = [
+    { name: 'selectData_1', id: 1 },
+    { name: 'selectData_2', id: 2 },
+  ];
+  public selectData_3: DropDownMenuData[] = [
+    { name: 'selectData_1', id: 1 }
+  ];
+  public selectData_4: DropDownMenuData[] = [
+    { name: 'selectData_1', id: 1 },
+    { name: 'selectData_2', id: 2 },
+    { name: 'selectData_3', id: 3 },
+  ];
+
+  ngOnInit(): void { }
 }
