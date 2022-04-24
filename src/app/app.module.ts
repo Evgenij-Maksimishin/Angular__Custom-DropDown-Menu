6a7c5819dc4a3fff44885a51a1d1a61329e214cd
Yevhenii Maksimishyn <evgenijmaksimishin@gmai.com>
2022-04-24 22:58:23 +0200

    Create drop down menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropDowmMenuComponent } from './components/drop-dowm-menu/drop-dowm-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropDowmMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/drop-dowm-menu/drop-dowm-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-dowm-menu',
+  templateUrl: './drop-dowm-menu.component.html',
+  styleUrls: ['./drop-dowm-menu.component.scss']
+})
+export class DropDowmMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
