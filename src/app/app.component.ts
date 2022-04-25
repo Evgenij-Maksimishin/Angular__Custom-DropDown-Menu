eb4f1de063d654c48560471bf350be92e3114922
Yevhenii Maksimishyn <evgenijmaksimishin@gmai.com>
2022-04-25 14:27:23 +0200

    Improve selectData_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,10 @@ export class AppComponent implements OnInit {
     { name: 'selectData_1', id: 1 },
     { name: 'selectData_2', id: 2 },
     { name: 'selectData_3', id: 3 },
-    { name: 'selectData_4', id: 4 }
+    { name: 'selectData_4', id: 4 },
+    { name: 'selectData_4', id: 5 },
+    { name: 'selectData_6', id: 6 },
+    { name: 'selectData_7', id: 7 }
   ];
   public selectData_2: DropDownMenuData[] = [
     { name: 'selectData_1', id: 1 },
