b5ef9861b43a2d907b4c9f848373af100cd51a03
Yevhenii Maksimishyn <evgenijmaksimishin@gmai.com>
2022-04-25 16:14:07 +0200

    Create OutsideClickDirective directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DropDowmMenuComponent } from './components/drop-dowm-menu/drop-dowm-menu.component';
+import { OutsideClickDirective } from './directives/outside-click.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DropDowmMenuComponent
+    DropDowmMenuComponent,
+    OutsideClickDirective
   ],
   imports: [
     BrowserModule,
